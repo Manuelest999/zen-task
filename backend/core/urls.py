9f728ab164dc2a6d303fbd2a698c9b726ae57cd6
Manuelest999 <manuel.est2002@gmail.com>
2026-06-05 18:37:12 -0400

    fix deploy 2

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from api.health import health_check
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/health/', health_check, name='health_check'),
     path('api/', include('api.urls')),
 ]
+
