6133dd28c2555130b39b072467537e3c91fc0fcb
Manuelest999 <manuel.est2002@gmail.com>
2026-06-05 18:19:39 -0400

    fix django

--- backend/core/asgi.py
+++ backend/core/asgi.py
@@ -9,15 +9,16 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
 
 import os
 import django
-from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-import api.routing
 
-from api.middleware import JWTAuthMiddleware
-
+# ⚠️ Estas dos líneas DEBEN ir antes de cualquier import de Django/app
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 django.setup()
+
+# Recién aquí importamos módulos que dependen de Django
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+import api.routing
+from api.middleware import JWTAuthMiddleware
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
